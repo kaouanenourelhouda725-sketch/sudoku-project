9e6bdae7f90e51cfe6036f295fb1dd320ad59d9f
hadouchedouaa5-beep <hadouchedouaa5@gmail.com>
2025-12-31 17:41:33 +0100

    Add user input : row, column, and number and "q" to  exit the loop
    We ask the user to input the number of row and column to specify the cell to place the number (variable)

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int grid[9][9];
-int i,j,row,col,n,val,ma_ja;
+int i,j,row,col,n,full,ma_ja;
 printf ("please enter the values of the sudoku grid(0 mean an emply cell)\n");
 for (i=0;i<9;i++){
    for (j=0;j<9;j++){
@@ -16,77 +16,23 @@ while(1){
         printf("\n");
     }
     full = 1;
+    if (full == 1) {
+        printf("the soduko grid is complete\n");
+        break;
+    }
     for(i = 0; i < 9; i++) {
         for (j = 0; j < 9; j++) {
             if (grid[i][j] == 0)
                 full = 0;
         }
     }
-    if (full == 1) {
-        printf("sudoko grid is complete\n");
+     printf("Enter row number (1-9) or (q:to quit): \n");
+    scanf("%d", &row);
+    if (row == "q")
         break;
-    }
 
-int isValid(int row, int col, int num) {
-    int i, j;
+    printf("Enter column number (1-9): \n");
+    scanf("%d", &col);
 
-    for (i = 0; i < 9; i++) {
-        if (grid[row][i] == num)
-            return 0;
-        if (grid[i][col] == num)
-            return 0;
-    }
-
-    int startRow = row - row % 3;
-    int startCol = col - col % 3;
-
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            if (grid[startRow + i][startCol + j] == num)
-                return 0;
-        }
-    }
-
-    return 1;
-}
-
-int solve() {
-    int row, col, num;
-
-    for (row = 0; row < 9; row++) {
-        for (col = 0; col < 9; col++) {
-            if (grid[row][col] == 0) {
-                for (num = 1; num <= 9; num++) {
-                    if (isValid(row, col, num)) {
-                        grid[row][col] = num;
-
-                        if (solve())
-                            return 1;
-
-                        grid[row][col] = 0;
-                    }
-                }
-                return 0;
-            }
-        }
-    }
-    return 1;
-}
-
-int main() {
-    int i, j;
-
-    if (solve()) {
-        printf("Sudoku Solved:\n");
-        for (i = 0; i < 9; i++) {
-            for (j = 0; j < 9; j++) {
-                printf("%d ", grid[i][j]);
-            }
-            printf("\n");
-        }
-    } else {
-        printf("No solution found\n");
-    }
-
-    return 0;
-}
+    printf("Enter number (1-9): ");
+    scanf("%d", &n);
