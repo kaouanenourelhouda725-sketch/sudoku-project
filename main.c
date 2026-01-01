82f14acb40701ef157cd03ad6ab6084517938925
kaouanenourelhouda725-sketch <kaouanenourelhouda725@gmail.com>
2026-01-02 04:56:19 +1300

    Fix input validation and Sudoku rules checking

--- main.c
+++ main.c
@@ -38,3 +38,42 @@ while(1){
     scanf("%d", &n);
     row--;
     col--;
+if (row < 0  row > 8  col < 0  col > 8  n< 1 || n > 9) {
+        printf(" Invalid input \n");
+        continue;
+    }
+    if (grid[row][col] != 0) {
+        printf(" This cell is already filled \n");
+        continue;
+    }
+
+   
+if (row < 0  row > 8  col < 0  col > 8  n< 1 || n > 9) {
+        printf(" Invalid input \n");
+        continue;
+    }
+    if (grid[row][col] != 0) {
+        printf(" This cell is already filled \n");
+        continue;
+    }
+valid = 1;
+    for (i = 0; i < 9; i++)
+        if (grid[row][i] == n)
+            valid = 0;
+    for (i = 0; i < 9; i++)
+        if (grid[i][col] == n)
+            valid = 0;
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            if (grid[startRow + i][startCol + j] == n)
+                valid = 0;
+
+    if (valid == 1) {
+        grid[row][col] = n;
+        printf(" Number placed successfully\n");
+    } else {
+        printf(" Number violates Sudoku rules\n");
+    }
+}
